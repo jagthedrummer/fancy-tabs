62c12b15b6fa6a6634f196f7922ffee9b7585853
Jeremy Green <jagthedrummer@gmail.com>
2011-01-25 00:43:14 -0600

    Finished the objectification.

--- js/fancy-tabs.js
+++ js/fancy-tabs.js
@@ -3,43 +3,62 @@
  if (typeof Effect == 'undefined')
   throw("You must have the script.aculo.us library to use FancyTabs. (https://github.com/jagthedrummer/fancy-tabs)");
 
-
-function FancyTabs(tabContainer,options){
-	this.id = "fancy-tabs-" + Math.floor(Math.random()*1000);
-	this.tabContainer = $(tabContainer);
-	this.options = {
-		classNames : {
-			handle : 'handle',
-			content : 'content'
-		},
-		onEvent : 'click'
-	};
-	this.tabs = $A();
-	
-	this.currentTab = null;
-	if(options != null){
-		Object.extend(this.options,options);
-	}
+var FancyTabs = Class.create({
 	
 	
-	this.initTabFrame = function(){
+	/*
+	 * Construct a new FancyTabs set
+	 * tabContainer : the container on the page that will get the tabs
+	 * options : options for the tabs
+	 */
+	initialize: function(tabContainer,options){
+		this.id = "fancy-tabs-" + Math.floor(Math.random()*1000);
+		this.tabContainer = $(tabContainer);
+		this.options = {
+			classNames : {
+				handle : 'handle',
+				content : 'content'
+			},
+			onEvent : 'click'
+		};
+		this.tabs = $A();
+		this.currentTab = null;
+		if(options != null){
+			Object.extend(this.options,options);
+		}
+		this.initTabFrame();
+		this.initContentFrame();
+		this.initDroppables();
+		this.initContainer();
+	},
+	
+	/*
+	 * Setup the container that will hold the actual tabs
+	 */
+	initTabFrame : function(){
 		this.tabFrame = new Element('ul', {'class': 'clearfix fancy-tab-frame','id':this.id+'-tab-frame'});
 		this.tabContainer.appendChild(this.tabFrame);
-	};
+	},
 	
-	this.initContentFrame = function(){
+	/*
+	 * Setup the container that will hold the content panels
+	 */
+	initContentFrame : function(){
 		this.contentFrame = new Element('div', {'class': 'clearfix fancy-content-frame','id':this.id+'-content-frame'});
 		this.tabContainer.appendChild(this.contentFrame);
-	};
+	},
 	
-	this.initDroppables = function(){
+	/*
+	 * Setup the droppable areas on the sides that will trigger a SplitPane
+	 */
+	initDroppables : function(){
 		this.droppableEast = new Element('div', {'class': 'fancy-droppable-east','id':this.id+'-droppable-east'});
 		this.contentFrame.appendChild(this.droppableEast);
 		Droppables.add(this.droppableEast,{hoverclass:'dropzone'})
-	};
+	},
 	
 	
-	this.initContainer = function(){
+	initContainer : function(){
 		handles = this.tabContainer.select("." + this.options.classNames.handle);
 		contents = this.tabContainer.select("." + this.options.classNames.content);
 		if(handles.length != contents.length){
@@ -50,23 +69,23 @@ function FancyTabs(tabContainer,options){
 			this.addTab(handles[i],contents[i])
 		}
 		this.setActiveTab(this.tabs.first());
-	}
+	},
 	
-	this.dragTargets = function(){
+	dragTargets : function(){
 		return [this.droppableEast,this.tabFrame]
-	}
+	},
 	
 	/*pass in the elements/content to be used as the tab handle/content*/
-	this.addTab = function(handle,content){
+	addTab : function(handle,content){
 		var tab = new FancyTab(handle,content,this);
 		this.tabFrame.appendChild(tab.tab_elem);
 		this.contentFrame.appendChild(tab.content_elem);
 		this.tabs.push(tab);
 		this.setActiveTab(tab);
 		Sortable.create(this.tabFrame.id,{overlap:'horizontal',constraint:false,containment:this.dragTargets()});
-	};
+	},
 	
-	this.closeTab = function(tab){
+	closeTab : function(tab){
 		console.log("removing for " + tab.handle_elem.innerHTML)
 		this.tabFrame.removeChild(tab.tab_elem);
 		this.contentFrame.removeChild(tab.content_elem);
@@ -79,9 +98,9 @@ function FancyTabs(tabContainer,options){
 		}else{
 			this.setActiveTab(null);
 		}
-	};
+	},
 	
-	this.setActiveTab = function(tab){
+	setActiveTab : function(tab){
 		console.log('activating ' + tab.handle_elem.innerHTML + " tabs.length = " + this.tabs.length);
 		if(this.currentTab!=null){
 			this.currentTab.hideTab();
@@ -90,13 +109,11 @@ function FancyTabs(tabContainer,options){
 		if(this.currentTab!=null){
 			this.currentTab.showTab();
 		}
-	};
+	}
 	
-	this.initTabFrame();
-	this.initContentFrame();
-	this.initDroppables();
-	this.initContainer();
-}
+})
+
+
 
 /*
  * This class represents the tabs themselves.
